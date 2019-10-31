e809b463e400787e8a4ebe60bd8a21a08808111e
maryfarner18 <maryfarner@gmail.com>
2019-10-31 16:12:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+let url = "http://localhost:3000/users"
+
+function appendData(obj){
+    let id = obj.id
+    // console.log("id: ", id)
+    // let p = document.createElement("p")
+    // p.innerText = id
+    // console.log("p: ", p)
+    // document.body.appendChild(p)
+    document.body.innerHTML += id;
+
+}
+
+function appendError(error){
+ 
+    document.body.innerHTML = error.message;
+
+}
+
+
+
+function submitData(name, email){
+    let obj = {
+        name: name,
+        email: email 
+    }
+    
+    return fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(obj)
+
+    })
+        .then(resp => resp.json())
+        .then(resp => appendData(resp))
+        .catch(err => appendError(err))
+}
