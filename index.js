5ec140825525051799d15415fd412f67a4234a9a
VPoshelyuk <aslava198@gmail.com>
2019-10-30 22:46:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    let data = {
+        name: userName,
+        email: userEmail
+    }
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(response => {
+        return response.json()
+      })
+      .then(object => {
+        document.body.innerHTML = object[ "id" ]
+        document.body.style.textAlign = "center"
+      })
+      .catch(error => {
+        alert("Bad things! Ragnarők!");
+        document.body.innerHTML = error.message
+        document.body.style.textAlign = "center"
+      })
+}
+// testing
+document.addEventListener('DOMContentLoaded', function() {
+    submitData("Slava", "not_areal_address@gmail.com")
+})
+// testing
