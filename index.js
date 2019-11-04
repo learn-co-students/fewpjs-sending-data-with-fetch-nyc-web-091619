23e43c41147dbe2eef5c491854c9f3e69e3c1c98
Danny Lee <dreamer@panix.com>
2019-11-03 21:53:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Add your code here
+const DOGPATH = "http://localhost:3000/dogs"
+
+// fetch(DOGPATH, {
+//     method: "POST"
+//     headers: {
+//         "Content-type":"application/json",
+//         "Accept":"application/json"
+//     }
+//     body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle"
+//     })
+
+// }
+
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+// }
+
+// let configObj = {
+//     method: "POST",
+//     headers: {
+//         "Content-type":"application/json",
+//         "Accept":"application/json"
+//     },
+//     body: JSON.stringify(formData)
+// };
+
+const USERPATH = "http://localhost:3000/users"
+
+
+function submitData(str1, str2) {
+    return fetch(USERPATH, {
+        method: "POST",
+        headers: {
+            "Content-type":"application/json",
+            "Accept":"application/json"
+        },
+        body: JSON.stringify({
+            name: str1,
+            email: str2
+        })
+    }).then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        document.querySelector("body").innerHTML+=object.id
+        console.log(object);
+    })
+    .catch(function(error) {
+        alert("Bad things!!");
+        document.querySelector("body").innerHTML+=error.message
+        console.log(error.message);
+    });
+}
